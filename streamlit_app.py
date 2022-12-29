8e2d6ad48cb943d12781b302dd1c2a4354cb247c
xionglei0515 <xionglei0515@gmail.com>
2022-12-29 21:53:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blue berry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard boiled Free-renage egg')
